022e28dad7a3104f021fe603a6b9b605258fe346
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-12-15 16:56:53 -0800

    Work on functionality

--- js/scripts.js
+++ js/scripts.js
@@ -12,77 +12,118 @@ cardTable.classList.add("card-table");
 const gameBoard = document.querySelector(".game-background");
 const allCards = document.querySelectorAll(".card");
 
-function toStart () {
+function toStart() {
   gameBoard.appendChild(menuScreen);
   cardTable.innerHTML = "";
   cardTable.remove();
   cardRoom.remove();
 }
 
-const card = (level) => {
-  const getCard = document.createElement("div");
-  const flipCardInner = document.createElement("div");
-  const flipCardFront = document.createElement("div");
-  const flipCardBack = document.createElement("div");
-  const imgValue = document.createElement("img");
-  const imgFront = document.createElement("img");
-  getCard.classList.add("card");
-  flipCardInner.classList.add("flip-card-inner");
-  flipCardFront.classList.add("flip-card-front");
-  flipCardBack.classList.add("flip-card-back");
-  imgValue.setAttribute("src", "../img/Gameover.png");
-  imgFront.setAttribute("src", "../img/Card1.png");
-  cardRoom.appendChild(getCard);
-  getCard.appendChild(flipCardInner);
-  flipCardInner.appendChild(flipCardFront);
-  flipCardInner.appendChild(flipCardBack);
-  flipCardFront.appendChild(imgFront);
-  flipCardBack.appendChild(imgValue);
-  flipCardInner.addEventListener("click", () => {
-    flipCardInner.classList.add("transform");
-    const allCards = document.querySelectorAll(".flip-card-inner");
-    console.log(allCards);
-    allCards.forEach((levelButton) => levelButton.addEventListener("click", toStart));  
-  });
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const chooseLevel = (elem) => {
-  allButtons.forEach((levelButton) => levelButton.classList.remove("checked"));
+  allButtons.forEach((level) => level.classList.remove("checked"));
   elem.target.classList.add("checked");
-}
+};
 
-allButtons.forEach((levelButton) => levelButton.addEventListener("click", chooseLevel));
+allButtons.forEach((level) => level.addEventListener("click", chooseLevel));
 
-
-function startGame () {
+function startGame() {
   let currentLevel = document.querySelector(".checked").getAttribute("id");
-  menuScreen.remove();
-  gameBoard.appendChild(cardRoom);
-  function createTable (level) {
-    cardRoom.classList.add("game-cards")
+  function createdTableLevel(level) {
+    cardRoom.classList.add("game-cards");
     switch (level) {
       case "easy":
-        cardRoom.classList.add("game-cards");
-        for(let i = 3; i-- ;) {
+        return 3;
+      case "medium":
+        return 6;
+      case "hard":
+        return 10;
+    }
+    return;
+  }
+
+  let newCreatedGame = createdTableLevel(currentLevel);
+  menuScreen.remove();
+  gameBoard.appendChild(cardRoom);
+  cardRoom.appendChild(cardTable);
+
+  let card = () => {
+    function createCards() {
+      const getCard = document.createElement("div");
+      const flipCardInner = document.createElement("div");
+      const flipCardFront = document.createElement("div");
+      const flipCardBack = document.createElement("div");
+      const imgValue = document.createElement("img");
+      const imgFront = document.createElement("img");
+      getCard.classList.add("card");
+      flipCardInner.classList.add("flip-card-inner", "hover-cards");
+      flipCardFront.classList.add("flip-card-front");
+      flipCardBack.classList.add("flip-card-back");
+      imgValue.setAttribute("src", "../img/Gameover.png");
+      imgFront.setAttribute("src", "../img/Card1.png");
+      cardTable.appendChild(getCard);
+      getCard.appendChild(flipCardInner);
+      flipCardInner.appendChild(flipCardFront);
+      flipCardInner.appendChild(flipCardBack);
+      flipCardFront.appendChild(imgFront);
+      flipCardBack.appendChild(imgValue);
+      
+      return {
+        createGameCards: getCard,
+        flipCardInnerName: flipCardInner,
+        flipCardFrontName: flipCardFront,
+        flipCardBackName: flipCardBack,
+        imgValueName: imgValue,
+        imgFrontName: imgFront,
+      };
+    }
+
+    let createdCards = createCards();
+
+    function getBugCard(levelButton) {
+      let randomBug = getRandom(1, levelButton);
+      if (randomBug == 1) {
+        createdCards.imgValueName.setAttribute("src", "../img/BugCard.png");
+      }
+    }
+
+    createdCards.flipCardInnerName.addEventListener("click", () => {
+      getBugCard(newCreatedGame);
+      createdCards.flipCardInnerName.classList.remove("hover-cards");
+      createdCards.flipCardInnerName.classList.add("transform");
+      const allCards = document.querySelectorAll(".flip-card-inner");
+      allCards.forEach((levelButton) =>
+        levelButton.addEventListener("click", toStart)
+      );
+    });
+  };
+
+  function createTable(levelButton) {
+    switch (levelButton) {
+      case "easy":
+        cardTable.classList.add("game-cards");
+        for (let i = 3; i--; ) {
           card(currentLevel);
         }
         break;
       case "medium":
-        cardRoom.classList.add("game-cards");
-        for(let i = 6; i-- ;) {
+        cardTable.classList.add("game-cards");
+        for (let i = 6; i--; ) {
           card(currentLevel);
         }
         break;
       case "hard":
-        cardRoom.classList.add("game-cards");
-        for(let i = 10; i-- ;) {
+        cardTable.classList.add("game-cards");
+        for (let i = 10; i--; ) {
           card(currentLevel);
         }
         break;
+    }
   }
+  createTable(currentLevel);
 }
-createTable(currentLevel);
-}
-
 
 startButton.addEventListener("click", startGame);
